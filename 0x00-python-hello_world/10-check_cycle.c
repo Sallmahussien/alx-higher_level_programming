15df5392865f85be2a3e9d3ab3ea2efab9e1ff6b
salma hussien <salmaa.hussien1@gmail.com>
2023-06-05 15:51:25 -0700

    cycle linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,41 +2,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-int search(listint_t *node, listint_t **arr, int arr_len);
-void append(listint_t *node, listint_t **arr, int *arr_len);
-
-/**
- * search - search for pointer node in array arr
- * @node: pointer to node
- * @arr: array
- * @arr_len: array length
- * Return: 1 if array has node, 0 otherwise
- */
-int search(listint_t *node, listint_t **arr, int arr_len)
-{
-	int i;
-
-	for (i = 0; i < arr_len; i++)
-		if (node == arr[i])
-			return (1);
-
-	return (0);
-
-}
-
-/**
- * append - append node at the end of an array
- * @node:
- * @arr:
- * @arr_len:
- */
-void append(listint_t *node, listint_t **arr, int *arr_len)
-{
-	arr =  realloc(arr, sizeof(listint_t *) * (*arr_len + 1));
-	arr[*arr_len ] = node;
-	(*arr_len)++;
-}
-
 /**
  * check_cycle - checks if linked list has a cycle or not
  * @list: linked list
@@ -44,24 +9,15 @@ void append(listint_t *node, listint_t **arr, int *arr_len)
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list;
-	listint_t **arr = malloc(sizeof(listint_t *));
-	int arr_len = 0;
+	listint_t *ptr1 = list;
+	listint_t *ptr2 = list;
 
-	while (current)
+	while (ptr1 && ptr2)
 	{
-		if (search(current, arr, arr_len) == 1)
-		{
-			free(arr);
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+		if (ptr1 == ptr2)
 			return (1);
-		}
-		else
-		{
-			append(current, arr, &arr_len);
-		}
-		current = current->next;
 	}
-
-	free(arr);
 	return (0);
 }
