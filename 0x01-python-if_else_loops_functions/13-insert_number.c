d1ce3c3d4a71b587f442c3a156c7135051fff788
salma hussien <salmaa.hussien1@gmail.com>
2023-06-06 14:52:58 -0700

    add node at start and end

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,26 +8,33 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *ptr1, *ptr2;
+	listint_t *ptr1 = *head, *ptr2;
 	listint_t *node = malloc(sizeof(listint_t));
 
 	if (node == NULL)
 		return (NULL);
-
 	node->n = number;
-
+	if (*head == NULL)
+	{
+		node->next = NULL;
+		*head = node;
+		return (node);
+	}
 	if ((*head)->n > number)
 	{
 		node->next = *head;
 		*head = node;
 		return (node);
 	}
-
-	ptr1 = *head;
-
 	while (ptr1)
 	{
 		ptr2 = ptr1->next;
+		if (ptr2 == NULL)
+		{
+			node->next = NULL;
+			ptr1->next = node;
+			return (node);
+		}
 		if (ptr2->n > number)
 		{
 			node->next = ptr2;
