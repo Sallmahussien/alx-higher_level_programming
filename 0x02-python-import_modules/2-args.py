ce1b4935506d89ead7157a4e8d9b916401845390
salma hussien <salmaa.hussien1@gmail.com>
2023-06-08 10:58:45 -0700

    solve an issue when arguments = 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,12 @@
 if __name__ == "__main__":
     import sys
 
-    for i in range(len(sys.argv)):
-        if i == 0:
-            print("{} arguments".format(len(sys.argv) - 1), end="")
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
 
-            if len(sys.argv) == 1:
-                print(".")
-                break
-            else:
-                print(":")
-                continue
-
+    for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
