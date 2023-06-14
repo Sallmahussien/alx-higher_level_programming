4ab532ea03886e7498dfad86d2042f75f5f56a7f
salma hussien <salmaa.hussien1@gmail.com>
2023-06-14 22:10:34 +0300

    Task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append(list(map(lambda num: num ** 2, row)))
+    return new_matrix
