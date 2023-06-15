5325ff58845db645d176d809e649ea3efffd01c3
salma hussien <salmaa.hussien1@gmail.com>
2023-06-15 04:51:08 +0300

    Edit Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 
 
 def complex_delete(a_dictionary, value):
-    if value in a_dictionary.values():
+    while value in a_dictionary.values():
         for key in a_dictionary:
             if a_dictionary[key] == value:
                 del a_dictionary[key]
