110aed56ce463aa267bec110f26c8ae00b2c3b68
salma hussien <salmaa.hussien1@gmail.com>
2023-07-02 21:30:26 +0300

    Add task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    printed_elements = 0
+
+    for idx in range(x):
+        try:
+            print(my_list[idx], end="")
+            printed_elements += 1
+        except IndexError:
+            break
+
+    print("")
+    return printed_elements
