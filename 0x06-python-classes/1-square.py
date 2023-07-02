98248f5c2f80f3949ce670c187680b41d723d220
salma hussien <salmaa.hussien1@gmail.com>
2023-07-02 19:57:57 +0300

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Define a class Square.'''
+
+
+class Square:
+    '''The implementation of a Square class'''
+    def __init__(self, size):
+        '''Initialize a new Square
+
+        Args:
+            size (int): Defines the size of new Square
+        '''
+        self.__size = size
