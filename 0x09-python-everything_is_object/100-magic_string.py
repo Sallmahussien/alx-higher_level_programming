a67f920662732b1a66c3cba543888a80ab87eaea
salma hussien <salmaa.hussien1@gmail.com>
2023-07-04 20:44:43 +0300

    Add task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ', '.join(['BestSchool'] * magic_string.count
+    return ', '.join(['BestSchool'] * magic_string.count)
