00150fbbcf10255bf70ce51ae4d6e5e106525314
salma hussien <salmaa.hussien1@gmail.com>
2023-07-10 19:09:39 +0300

    Edit pycodestyle on task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     '''Returns True if the object is an instance of a class that inherited
-       (directly or indirectly) from the specified class; 
+       (directly or indirectly) from the specified class;
        otherwise False
     '''
     if type(obj) == a_class:
