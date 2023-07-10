2c446ca1d5d02e3bdd84f92ddaa6b9ce41d8d660
salma hussien <salmaa.hussien1@gmail.com>
2023-07-10 21:17:38 +0300

    Add task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Defines Square class'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Implement Square class'''
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        '''Returns a printable str'''
+        return '[Square] {}/{}'.format(self.__size, self.__size)
