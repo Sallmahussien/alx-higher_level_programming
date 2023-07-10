81eee655847e77a34503f9b5456e74c23cf1b60e
salma hussien <salmaa.hussien1@gmail.com>
2023-07-10 19:15:53 +0300

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Defines BaseGeometry class'''
+
+
+class BaseGeometry:
+    '''The implementation of BaseGeometry class'''
+    def area(self):
+        '''Calculate the area of the BaseGeometry instance'''
+        raise Exception('area() is not implemented')
