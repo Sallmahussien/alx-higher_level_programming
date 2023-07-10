7a2d372522ec9f2a747bc306e1a40ba91c226de0
salma hussien <salmaa.hussien1@gmail.com>
2023-07-10 21:02:38 +0300

    Add task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,4 +18,5 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
+        '''Returns a printable str'''
         return '[Rectangle] {}/{}'.format(self.__width, self.__height)
