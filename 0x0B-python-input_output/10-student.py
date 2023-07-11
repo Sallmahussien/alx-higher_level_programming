99d989cf823f3d26a3a57fe61c00edfb0654b901
salma hussien <salmaa.hussien1@gmail.com>
2023-07-11 22:38:46 +0300

    Add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Defines Student class'''
+
+
+class Student:
+    '''Implement Student class'''
+    def __init__(self, first_name, last_name, age):
+        '''Initialize a new instance from Student class'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Retrieves a dictionary representation of a Student instance'''
+        if attrs is None or not all(isinstance(ele, str) for ele in attrs):
+            return self.__dict__
+
+        new_dict = {}
+        for key in attrs:
+            if hasattr(self, key):
+                new_dict[key] = getattr(self, key)
+        return new_dict
