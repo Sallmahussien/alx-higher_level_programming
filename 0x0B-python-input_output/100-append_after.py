34956378dfe302dfec6b8e46fc42684af6e73508
salma hussien <salmaa.hussien1@gmail.com>
2023-07-12 00:17:32 +0300

    Add task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Defines append_after function'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''Inserts a line of text to a file,
+       after each line containing a specific string
+    '''
+
+    with open(filename, encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for idx, line in enumerate(lines):
+        if search_string in line:
+            lines.insert(idx + 1, new_string)
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
