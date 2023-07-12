7197b1b98614ebf887c6b2f3dfb8c4b74872ec64
salma hussien <salmaa.hussien1@gmail.com>
2023-07-12 05:12:17 +0300

    Edit task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -23,14 +23,14 @@ if __name__ == "__main__":
                 print_info(size, code_dict)
                 count = 1
             else:
-              count += 1
+                count += 1
             line_list = line.split()
             size += int(line_list[-1])
             if line_list[-2] in code_dict:
                 code_dict[line_list[-2]] += 1
             else:
                 if line_list[-2] in codes:
-                  code_dict[line_list[-2]] = 1
+                    code_dict[line_list[-2]] = 1
     except KeyboardInterrupt:
         print_info(size, code_dict)
         raise
