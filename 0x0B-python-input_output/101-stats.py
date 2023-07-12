49df49d303d2f10e69e31b8f392d489520d797e2
salma hussien <salmaa.hussien1@gmail.com>
2023-07-12 05:10:56 +0300

    Edit task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,17 +15,22 @@ if __name__ == "__main__":
     count = 0
     size = 0
     code_dict = {}
+    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
-    for line in sys.stdin:
-        if count == 10:
-            print_info(size, code_dict)
-            count = 0
-            size = 0
-            code_dict = {}
-        count += 1
-        line_list = line.split()
-        size += int(line_list[-1])
-        if line_list[-2] in code_dict:
-            code_dict[line_list[-2]] += 1
-        else:
-            code_dict[line_list[-2]] = 1
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_info(size, code_dict)
+                count = 1
+            else:
+              count += 1
+            line_list = line.split()
+            size += int(line_list[-1])
+            if line_list[-2] in code_dict:
+                code_dict[line_list[-2]] += 1
+            else:
+                if line_list[-2] in codes:
+                  code_dict[line_list[-2]] = 1
+    except KeyboardInterrupt:
+        print_info(size, code_dict)
+        raise
