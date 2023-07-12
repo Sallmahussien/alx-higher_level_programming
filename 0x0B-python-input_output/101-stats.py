cedfaab8a0afd28808a27f5227ea26e9cc9502ff
salma hussien <salmaa.hussien1@gmail.com>
2023-07-12 05:43:56 +0300

    Edit task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,37 +1,48 @@
-#!/usr/bin/python3
-'''Reads stdin line by line and computes metrics'''
-import sys
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
-
-def print_info(size, code_dict):
-    '''Prints some info'''
-    print('File size: {:d}'.format(size))
-
-    for key in sorted(code_dict):
-        print('{}: {:d}'.format(key, code_dict[key]))
-
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
-    count = 0
+    import sys
+
     size = 0
-    code_dict = {}
-    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
     try:
         for line in sys.stdin:
             if count == 10:
-                print_info(size, code_dict)
+                print_stats(size, status_codes)
                 count = 1
             else:
                 count += 1
-            line_list = line.split()
-            size += int(line_list[-1])
-            if line_list[-2] in code_dict:
-                code_dict[line_list[-2]] += 1
-            else:
-                if line_list[-2] in codes:
-                    code_dict[line_list[-2]] = 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
-        print_info(size, code_dict)
+        print_stats(size, status_codes)
         raise
