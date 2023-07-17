5e31de8060256528ae9b28bbc9de8cfb8be79063
salma hussien <salmaa.hussien1@gmail.com>
2023-07-17 19:14:24 +0300

    Edit pycodestyle on square file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,13 +30,13 @@ class Square(Rectangle):
         args_len = len(args)
         if args_len != 0:
             if args_len >= 1:
-              self.id = args[0]
+                self.id = args[0]
             if args_len >= 2:
-              self.width = args[1]
+                self.width = args[1]
             if args_len >= 3:
-              self.x = args[2]
+                self.x = args[2]
             if args_len >= 4:
-              self.y = args[3]
+                self.y = args[3]
         elif len(kwargs) != 0:
             for key, value in kwargs.items():
                 setattr(self, key, value)
