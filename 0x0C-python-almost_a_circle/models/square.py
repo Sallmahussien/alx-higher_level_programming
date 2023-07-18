0a81a29d1c37fd8c180881393c460e233c1f3953
salma hussien <salmaa.hussien1@gmail.com>
2023-07-18 06:53:51 +0300

    Fix to_dictionary method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,4 +43,4 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         '''Returns the dictionary representation of a Square'''
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
