f0cdcfb574dfc1e6af793de2d75b53a09463d1d1
salma hussien <salmaa.hussien1@gmail.com>
2023-09-12 23:51:26 +0300

    Add task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+
+for (const key in dict) {
+  if (!newDict[dict[key].toString()]) {
+    newDict[dict[key].toString()] = [];
+  }
+  newDict[dict[key].toString()].push(key);
+}
+
+console.log(dict);
+console.log(newDict);
