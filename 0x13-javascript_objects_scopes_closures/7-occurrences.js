41ee638de1685ae6f336e994da3000f8ba379480
salma hussien <salmaa.hussien1@gmail.com>
2023-09-12 22:31:00 +0300

    Add task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+exports.nbOccurences = function (list, searchElement) {
+  let occurences = 0;
+
+  list.forEach(element => {
+    if (element === searchElement) {
+      occurences++;
+    }
+  });
+  return occurences;
+};
