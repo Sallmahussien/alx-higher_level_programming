08550c20a8452879d3899f93c56ec321e9307cd9
salma hussien <salmaa.hussien1@gmail.com>
2023-09-16 09:22:51 +0300

    Add task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                          db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name"
-                "FROM cities LEFT JOIN states ON"
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM cities INNER JOIN states ON "
                 "cities.state_id = states.id ORDER BY cities.id ASC")
 
     rows = cur.fetchall()
