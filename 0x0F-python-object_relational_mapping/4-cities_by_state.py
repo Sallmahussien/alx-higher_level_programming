024c998016030a8c2ccd923f2f40939fd767ab93
salma hussien <salmaa.hussien1@gmail.com>
2023-09-16 09:20:16 +0300

    Add task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists all cities from the database hbtn_0e_4_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name"
+                "FROM cities LEFT JOIN states ON"
+                "cities.state_id = states.id ORDER BY cities.id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
