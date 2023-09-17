ba0ac6e846465a6562ed4b4a4771871f6416e270
salma hussien <salmaa.hussien1@gmail.com>
2023-09-17 20:36:04 +0300

    Fix pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Contains the class definition of a State and an instance Base = declarative_base():"""
+"""Contains the class definition of a State
+   and an instance Base = declarative_base():
+   """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Represents a state table for a MySQL database."""
     __tablename__ = 'states'
     id = Column("id", Integer, primary_key=True)
-    name = Column("name", String(128 ), nullable=False)
+    name = Column("name", String(128), nullable=False)
