cfdcd4e68b31fc0b0617d7bb0e88f2b53c18c7db
salma hussien <salmaa.hussien1@gmail.com>
2023-09-29 00:43:24 +0300

    Add task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX OPTIONS "$1" | awk '/Allow/ {print $2}'
+curl -sI -X OPTIONS "$1" | awk '/Allow/ {print $2}'
