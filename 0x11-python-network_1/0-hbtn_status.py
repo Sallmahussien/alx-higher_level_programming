ff532657b1cc098184b0e80b87ce9d5aa5e248fe
salma hussien <salmaa.hussien1@gmail.com>
2023-09-29 20:10:54 +0300

    Add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches a given url using urllib"""
+
+from urllib.request import urlopen
+
+
+with urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode("utf-8")))
