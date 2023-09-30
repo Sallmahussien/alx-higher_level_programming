83d93e86f8dec0e216ff25ac2657a29b9946f50f
salma hussien <salmaa.hussien1@gmail.com>
2023-09-30 03:23:31 +0300

    Add the adv. task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Lists the latest 10 commits of a specific repository by a specific user,
+    using the GitHub API
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
+
+    response = requests.get(url).json()
+
+    commits_list = []
+    for commit in response:
+
+        commit_dict = {
+            "sha": commit.get("sha"),
+            "author": commit.get("commit").get("author").get("name"),
+            "time": commit.get("commit").get("author").get("date")
+        }
+        commits_list.append(commit_dict)
+
+    sorted_commits = sorted(commits_list,
+                            key=lambda x: x['time'],
+                            reverse=True)
+
+    for idx in range(min(10, len(sorted_commits))):
+        sha = sorted_commits[idx].get("sha")
+        author = sorted_commits[idx].get("author")
+        print("{}: {}".format(sha, author))
