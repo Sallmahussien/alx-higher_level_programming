27df5ef3914ea9e238a27f85541484a5f72ff9a5
salma hussien <salmaa.hussien1@gmail.com>
2023-10-24 19:19:27 +0300

    Add task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const fs = require('node:fs');
+
+fs.readFile(args[2], 'utf-8', (error, data) => {
+  if (error) console.log(error);
+  else console.log(data);
+});
