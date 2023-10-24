60436bc1e4e669a39c36ad96e5ea58497342099f
salma hussien <salmaa.hussien1@gmail.com>
2023-10-24 21:09:53 +0300

    Add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,8 @@ const request = require('request');
 const characterURL = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(args[2], (error, response, body) => {
-  if (!error) {
+  if (error) console.error(error)
+  else {
     let moviesCount = 0;
     const moviesData = JSON.parse(body).results;
 
